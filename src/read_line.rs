3b70a6b0a20420231b22eb8296313860cb706982
Thomas Bollmeier <developer@thomas-bollmeier.de>
2025-02-08 09:51:39 +0100

    codecrafters submit [skip ci]

--- src/read_line.rs
+++ src/read_line.rs
@@ -1,7 +1,7 @@
-use std::io::{stdin, stdout, Write};
+use std::io::{stdin, stdout, Stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+use termion::raw::{IntoRawMode, RawTerminal};
 
 pub type TabCompletion = fn (prefix: &str) -> Vec<String>;
 
@@ -36,27 +36,21 @@ pub fn read_line(prompt: &str, tab_completion: TabCompletion) -> String {
                                 print!("{}", buffer);
                             }
                             _ => {
-                                let bell = '\x07';
-                                print!("{}", bell);
+                                let common_prefix = find_common_prefix(&commands);
+                                if common_prefix.len() > buffer.len() {
+                                    for c in common_prefix.chars().skip(buffer.len()) {
+                                        buffer.push(c);
+                                        print!("{}", c);
+                                    }
+                                    commands.clear();
+                                } else {
+                                    let bell = '\x07';
+                                    print!("{}", bell);
+                                }
                             }
                         }
                     } else {
-                        let offset = buffer.len() + prompt.len();
-                        write!(
-                            stdout,
-                            "{}{}",
-                            termion::cursor::Left(offset as u16),
-                            termion::cursor::Down(1),
-                        ).unwrap();
-                        let commands_str = commands.join("  ");
-                        write!(stdout,
-                               "{}{}{}{}{}",
-                               commands_str,
-                               termion::cursor::Down(1),
-                               termion::cursor::Left(commands_str.len() as u16),
-                               prompt,
-                               buffer,
-                        ).unwrap();
+                        show_matching_commands(&mut stdout, prompt, &buffer, &commands);
                         commands.clear();
                     }
                 },
@@ -81,4 +75,73 @@ pub fn read_line(prompt: &str, tab_completion: TabCompletion) -> String {
     println!();
 
     buffer
+}
+
+fn goto_begin_of_line(
+    stdout: &mut RawTerminal<Stdout>,
+    prompt: &str,
+    buffer: &str) {
+    let offset = prompt.len() + buffer.len();
+    write!(stdout, "{}", termion::cursor::Left(offset as u16)).unwrap();
+}
+
+fn show_matching_commands(
+    stdout: &mut RawTerminal<Stdout>,
+    prompt: &str,
+    buffer: &str,
+    commands: &Vec<String>) {
+    goto_begin_of_line(stdout, prompt, buffer);
+    write!(
+        stdout,
+        "{}",
+        termion::cursor::Down(1),
+    ).unwrap();
+    let commands_str = commands.join("  ");
+    write!(stdout,
+           "{}{}{}{}{}",
+           commands_str,
+           termion::cursor::Down(1),
+           termion::cursor::Left(commands_str.len() as u16),
+           prompt,
+           buffer,
+    ).unwrap();
+}
+
+fn find_common_prefix(strings: &Vec<String>) -> String {
+    let mut prefix: Vec<char> = vec![];
+    let mut first = true;
+
+    for s in strings {
+        if first {
+            prefix = s.chars().collect();
+            first = false;
+            continue;
+        }
+
+        for (i, c) in s.chars().enumerate() {
+            if i >= prefix.len() || c != prefix[i] {
+                prefix.truncate(i);
+                break;
+            }
+        }
+    }
+
+    prefix.iter().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_common_prefix() {
+        let commands = vec![
+            "xyz_foo".to_string(),
+            "xyz_foo_bar".to_string(),
+            "xyz_foo_bar_baz".to_string(),
+        ];
+
+        assert_eq!(find_common_prefix(&commands), String::from("xyz_foo"));
+    }
+
 }
